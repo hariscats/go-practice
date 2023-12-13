d4cc2a46d4df0806c61072f36702536e55930867
Haris Catakovic <hariscats@gmail.com>
2023-12-12 20:12:54 -0500

    Do ch03 exercises

---
+++ ch03/compare.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("There is a sign near the entrance that reads 'No Minors'.")
+
+	var age = 42
+	var minor = age < 18
+
+	fmt.Printf("At age %v, am I a minor? %v\n", age, minor)
+}
---
+++ ch03/if.go
@@ -0,0 +1,15 @@
+package main // Declare package name
+
+import "fmt"
+
+func main() {
+	var command = "go inside"
+
+	if command == "go east" {
+		fmt.Println("You head further up the mountain.")
+	} else if command == "go inside" {
+		fmt.Println("You enter the cave where you live out the rest of your life.")
+	} else {
+		fmt.Println("Didn't quite get that.")
+	}
+}
