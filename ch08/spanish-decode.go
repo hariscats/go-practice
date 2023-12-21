278be556068c73ae072f22ff5d5041e4d913274d
Haris Catakovic <hariscats@gmail.com>
2023-12-21 12:01:21 -0500

    Finish ch08 with UTF8 exercise

---
+++ ch08/spanish-decode.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+	question := "¿Cómo estás?"
+
+	fmt.Println(len(question), "bytes")
+	fmt.Println(utf8.RuneCountInString(question), "runes")
+
+	c, size := utf8.DecodeRuneInString(question)
+	fmt.Printf("First rune: %c %v bytes", c, size)
+
+	// range keyword iterates over runes
+	for _, c := range question {
+		fmt.Printf("%c ", c)
+	}
+}
