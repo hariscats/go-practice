1acea8ade8060e90f871e3053b04b7bbacb24e39
Haris Catakovic <hariscats@gmail.com>
2024-01-13 15:54:53 -0500

    Finish Conway's game of life

--- ch19/conways-game.go
+++ ch19/conways-game.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 const (
@@ -38,5 +39,51 @@ func (u Universe) Seed() {
 		x := rand.Intn(width)
 		y := rand.Intn(height)
 		u[y][x] = true
+	}
+}
+
+func (u Universe) Alive(x, y int) bool {
+	x = (x + width) % width
+	y = (y + height) % height
+	return u[y][x]
+}
+
+func (u Universe) Neighbors(x, y int) int {
+	count := 0
+	for i := -1; i <= 1; i++ {
+		for j := -1; j <= 1; j++ {
+			if !(j == 0 && i == 0) && u.Alive(x+i, y+j) {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func (u Universe) Next(x, y int) bool {
+	alive := u.Alive(x, y)
+	neighbors := u.Neighbors(x, y)
+
+	return neighbors == 3 || alive && neighbors == 2
+}
+
+func Step(a, b Universe) {
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			b[y][x] = a.Next(x, y)
+		}
+	}
+}
+
+func main() {
+	a, b := NewUniverse(), NewUniverse()
+	a.Seed()
+
+	for i := 0; i < 10; i++ {
+		Step(a, b)
+		fmt.Print("\x0c") // Clear screen
+		b.Show()
+		time.Sleep(time.Second / 2)
+		a, b = b, a // Swap universes
 	}
 }
