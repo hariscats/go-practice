ee556da2d2a007013dac63e4b62eb16a56abc827
Haris Catakovic <hariscats@gmail.com>
2024-01-13 15:42:42 -0500

    Progress on capstone

--- ch19/conways-game.go
+++ ch19/conways-game.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 const (
 	width  = 80
 	height = 15
@@ -13,4 +18,25 @@ func NewUniverse() Universe {
 		u[i] = make([]bool, width)
 	}
 	return u
+}
+
+func (u Universe) Show() {
+	for _, row := range u {
+		for _, cell := range row {
+			if cell {
+				fmt.Print("*")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func (u Universe) Seed() {
+	for i := 0; i < (width*height)/4; i++ {
+		x := rand.Intn(width)
+		y := rand.Intn(height)
+		u[y][x] = true
+	}
 }
